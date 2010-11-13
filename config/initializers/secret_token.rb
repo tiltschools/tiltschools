63ca68ca6e9d908ced693315a2b950b58b0c5758
Jared Cosulich <jaredcosulich@Jared-Cosulichs-MacBook-Pro.local>
2010-11-13 10:42:00 -0800

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tiltschoolst::Application.config.secret_token = 'c9eab913cf11fc34f31075cbdcc578f808c321d2ada944a52da83ab01287e6d98889100a681cb0cff8c63b7937af3ba44760747cb0c08b614d4b00cfe6849dfb'
